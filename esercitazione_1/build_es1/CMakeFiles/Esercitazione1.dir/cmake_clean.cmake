f1bd24dcf076ed1e335ec651ef8d541d27ba7adb
Luca Barla <balurc.32@gmail.com>
2026-03-11 15:18:17 +0000

    Consegna finale esercitazione 1

---
+++ esercitazione_1/build_es1/CMakeFiles/Esercitazione1.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Esercitazione1.dir/link.d"
+  "CMakeFiles/Esercitazione1.dir/es_1.cpp.o"
+  "CMakeFiles/Esercitazione1.dir/es_1.cpp.o.d"
+  "CMakeFiles/Esercitazione1.dir/printargs.cpp.o"
+  "CMakeFiles/Esercitazione1.dir/printargs.cpp.o.d"
+  "Esercitazione1"
+  "Esercitazione1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Esercitazione1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
